3173be32a1171bff7374b3dc2f9b1e466c94da8b
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-06-20 14:30:49 -0300

    tela de cliente

--- src/pages/Home/HomeCliente/HomeCliente.jsx
+++ src/pages/Home/HomeCliente/HomeCliente.jsx
@@ -1,13 +1,31 @@
 import Navbar from "../../../components/Navbar/Navbar";
-import { Container } from "./HomeClienteStyled";
+import { Container, MesaLista, Opcoes } from "./HomeClienteStyled";
 
 function HomeCliente(){
     return(
         <>
             <Navbar />
-            <Container>
-                
-            </Container>
+                <Container>
+                    <MesaLista>
+                        <div> 
+                            <h2>Lista da Mesa </h2>
+                        </div>
+
+                        <p>item</p>
+                        <p>item</p>
+                        <p>item</p>
+                    </MesaLista>
+
+                    <Opcoes>
+                        <div> 
+                            <h2>Opções</h2>
+                        </div>
+
+                        <p>opção</p>
+                        <p>opção</p>
+                        <p>opção</p>
+                    </Opcoes>
+                </Container>
         </>
     )
 }
--- src/pages/Home/HomeCliente/HomeClienteStyled.jsx
+++ src/pages/Home/HomeCliente/HomeClienteStyled.jsx
@@ -4,6 +4,60 @@ export const Container = styled.section`
     display: flex;
     flex-direction: column;
     max-width: 100%;
+    margin-top: 2rem;
+
+    background-color: #fff;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-    background-color: red;
+    
+`;
+
+export const MesaLista = styled.section`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+    text-align: center;
+    padding: 1rem;
+
+    div {
+        display: flex;
+        flex-direction: column;
+    }
+
+    div h2 {
+        flex-direction: row;
+        justify-content: space-between;
+        
+    }
+
+    p {
+        flex-direction: row;
+        justify-content: space-between;
+        margin-top: 10px;
+    }
+`;   
+
+export const Opcoes = styled.section`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+    text-align: center;
+    padding: 1rem;
+
+    div {
+        display: flex;
+        flex-direction: column;
+    }
+
+    div h2{
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    p {
+        flex-direction: row;
+        justify-content: space-between;
+        margin-top: 10px;
+    }
 `;
